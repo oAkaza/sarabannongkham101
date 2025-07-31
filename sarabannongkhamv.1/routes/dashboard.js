5a3abc568a7535b06e7e86eaaa289905a762256f
oAkaza <openguineiei@gmail.com>
2025-07-31 09:36:11 +0700

    Delete sarabannongkhamv.1 directory

--- sarabannongkhamv.1/routes/dashboard.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  if (!req.session.user) return res.redirect('/');
-  res.render('dashboard', { user: req.session.user });
-});
-
-module.exports = router;
